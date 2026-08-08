f3909c4b449aff9c1fe25018dbccfbc3890774ba
musaibshowkat <25052031@kiit.ac.in>
2026-08-08 23:44:20 +0530

    Add files via upload

---
+++ areaofCircle.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main(){
+int r=6;// !@#$%^&*(){}:'.,/"" these cannot be used as these are special characters except underscore
+float a=3.14*r*r;
+printf("%f",a);
+ return 0;   
+}
